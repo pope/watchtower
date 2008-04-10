8abb7b5c4fec634a04e434a944293f9a775cf035
Adam <pope@shifteleven.com>
2008-04-10 18:13:00 -0400

    Starting a new GAE project

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import wsgiref.handlers
+from google.appengine.ext import webapp
+
+class MainHandler(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('Hello world!')
+
+def main():
+  application = webapp.WSGIApplication([('/', MainHandler)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+  main()
